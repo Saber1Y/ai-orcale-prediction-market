4151a922043fb491c76cd9a795c7bc46f4054f0e
Saber1Y <ayodejiakintobi1@gmail.com>
2026-03-18 12:43:36 +0100

    feat: fix aggregate + oracle files

--- src/OracleAgreegator.sol
+++ src/OracleAgreegator.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.33;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract OracleAggregator is Ownable {
     constructor(address initialOwner) Ownable(initialOwner) {
--- src/PredictionMarket.sol
+++ src/PredictionMarket.sol
@@ -3,22 +3,22 @@ pragma solidity ^0.8.33;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import  "forge-std/console.sol";
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import  {console} from "forge-std/console.sol";
 
 error PleaseInputQuestion();
 error CreatedMarketMustBeInTheFuture();
 
 
-contract PredictionMarket is Ownable {
+contract PredictionMarket is Ownable, ReentrancyGuard {
     address public admin;
     IERC20 public usdc;
     uint256 platformFee = 200; // converted to 2% in sol
 
     mapping (uint256 => MarketDetails) markets; //markets[1] created a index of struct data
-    mapping(uint256 => mapping (address => uint256)) public YesBets; //how much of yes bets did user 0x123 for market[1] YesBets[1]
-    mapping(uint256 => mapping (address => uint256)) public NoBets; //how much of NO bets did user 0x123 for market[1] NoBets[1]
+    mapping(uint256 => mapping (address => uint256)) public yesBets; //how much of yes bets did user 0x123 for market[1] YesBets[1]
+    mapping(uint256 => mapping (address => uint256)) public noBets; //how much of NO bets did user 0x123 for market[1] NoBets[1]
     mapping(uint256 => mapping(address => bool)) public claimed; 
 
     //create oracle aggregator contract later to pass 
